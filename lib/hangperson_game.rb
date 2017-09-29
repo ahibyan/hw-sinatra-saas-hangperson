15298f8337c1d6b2d82550c229183f044453c700
ahibyan <ahibyan@uncc.edu>
2017-09-29 15:33:14 +0000

    Everything but win or lose

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,15 +1,52 @@
 class HangpersonGame
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
-  
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ""
+    @wrong_guesses = ""
+    @word_with_guesses =""
+    
+  end
+
+  def guess(letter)
+   raise ArgumentError if not letter =~ /^[a-zA-z]$/
+    
+    letter.downcase!
+    return false if guesses.include? letter
+    return false if wrong_guesses.include? letter
+    if word.include? letter and not guesses.include? letter
+      guesses<< letter 
+      return true
+     else
+      wrong_guesses << letter
+      return true
+    end
+  end
+  def word_with_guesses
+    if guesses.empty? 
+     '-' * word.length 
+    else
+      word.gsub(/[^#{guesses}]/, '-')
+    end
+  end
+  
+  def check_win_or_lose
+    return :lose if wrong_guesses.length == 7
+    return :win if word =~ /[^#{guesses}]/
+    :play
+  end
+  
+  def word()
+    @word 
+  end
+
+  def wrong_guesses()
+    @wrong_guesses
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
