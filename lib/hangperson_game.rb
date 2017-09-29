772686ddeb343a41d6e0e5db2f52b4f1609a88c4
ahibyan <ahibyan@uncc.edu>
2017-09-29 16:00:08 +0000

    Check win and lose now working

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -37,18 +37,10 @@ class HangpersonGame
   
   def check_win_or_lose
     return :lose if wrong_guesses.length == 7
-    return :win if word =~ /[^#{guesses}]/
-    :play
+    return :win if !(word_with_guesses.include?('-'))
+    return :play
   end
-  
-  def word()
-    @word 
-  end
-
-  def wrong_guesses()
-    @wrong_guesses
-  end
-
+ 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
@@ -60,5 +52,4 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
-
 end
